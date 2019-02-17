b95a0845e7a0736505052b6068e631550329152f
Molly <mkc35@duke.edu>
2019-02-17 15:01:08 -0500

    Final Commit project worked on together by Kaitlyn and Molly with HTML, CSS, and JS

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+    $(function(){
+        'use-strict';
+       $('#colorToggle').click(function(){
+           $('.websiteChange').toggleClass("black");
+       })
+        
+        
+    });
